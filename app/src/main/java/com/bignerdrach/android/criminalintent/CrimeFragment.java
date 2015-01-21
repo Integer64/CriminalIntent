96595285366a22269640e247b525a42b06d198c7
Integer64 <tivan@bk.ru>
2015-01-21 23:53:54 +0800

    chapter 16

--- app/src/main/java/com/bignerdrach/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdrach/android/criminalintent/CrimeFragment.java
@@ -1,16 +1,20 @@
 package com.bignerdrach.android.criminalintent;
 
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -44,15 +48,22 @@ public class CrimeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         UUID crimeId = (UUID)getArguments().getSerializable(EXTRA_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+        setHasOptionsMenu(true);
     }
 
     public void updateDate(){
         mDateButton.setText(DateFormat.format("EEEE, MMM dd, HH:MM, yyyy",mCrime.getDate()));
     }
-
+    @TargetApi(11)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+            if (NavUtils.getParentActivityName(getActivity())!=null) {
+                getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
+            }
+        }
 
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
         mTitleField.setText(mCrime.getTitle());
@@ -133,6 +144,19 @@ public class CrimeFragment extends Fragment {
             Date date = (Date)data.getSerializableExtra(TimePickerFragment.EXTRA_TIME);
             mCrime.setDate(date);
             updateDate();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                if(NavUtils.getParentActivityName(getActivity())!=null){
+                    NavUtils.navigateUpFromSameTask(getActivity());
+                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
--- app/src/main/java/com/bignerdrach/android/criminalintent/SingleFragmentActivity.java
+++ app/src/main/java/com/bignerdrach/android/criminalintent/SingleFragmentActivity.java
@@ -1,10 +1,10 @@
 package com.bignerdrach.android.criminalintent;
 
-
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+
 
 public abstract class SingleFragmentActivity extends FragmentActivity{
     protected abstract Fragment createFragment();
@@ -13,6 +13,7 @@ public abstract class SingleFragmentActivity extends FragmentActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
         FragmentManager fm = getSupportFragmentManager();
         Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
         if(fragment == null){
